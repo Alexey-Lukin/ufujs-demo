930c699962a7ba2a558197d631140173a129f510
Alexey-Lukin <lukin@active-bridge.com>
2015-09-04 11:35:42 +0300

    Save decoded image

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,15 @@ class CommentsController < ApplicationController
   end
 
   def create
-    binding.pry
+    image = params[:comment][:image]
+    if image.is_a?(Array)
+      image.each do |i|
+        File.open(i.original_filename, "wb") { |f| f.write(i.read) }
+      end
+    else
+      File.open(image.original_filename, "wb") { |f| f.write(image.read) }
+    end
+    @comment = Comment.create(comment_params)
   end
 
   def edit
